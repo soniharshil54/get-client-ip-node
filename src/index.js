4c6e4dad7a7a0ade1ae412dd41682c6c795a6a8d
soniharshil54 <sharshil43@yahoo.com>
2024-08-25 23:36:31 +0530

    cool

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const healthcheck = async (req, res, next) => {
         return res.json({
             userInfo: response,
             message: 'ok',
-            note: 'ok man yoo'
+            note: 'ok man yoop'
         });
     } catch (err) {
         console.log('err', err);
