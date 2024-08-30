8c19e9f96c787d8dd1c83bf0dd5d2d440331c9cb
soniharshil54 <sharshil43@yahoo.com>
2024-08-30 22:11:08 +0530

    chore: Update note in healthcheck response

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const healthcheck = async (req, res, next) => {
         return res.json({
             userInfo: response,
             message: 'ok',
-            note: 'ok man yoop poop noon'
+            note: 'ok man yoop poop noona'
         });
     } catch (err) {
         console.log('err', err);
